5e67fbeffa116a9133f5fea9e4abd595c06edf70
datajav <156545837+datajav@users.noreply.github.com>
2026-03-24 21:14:57 -0500

    Update chunker.py

--- ingestion/chunker.py
+++ ingestion/chunker.py
@@ -6,5 +6,7 @@ SECTION_PATTERNS = [
     re.compile(r"^(\d+\.(?:d+\.?)*)\s+([A-Z][^\n]{0,80}})", re.multiline), 
 
     re.compile(r"^(Article|Section|ARTICLE|SECTION)\s+(\d+\.?\d*\.?\d*)\s*[:\--]?\s*([A-Z][^\n]{0,80})?", re.MULTILINE), 
+
+    
     
 ]
